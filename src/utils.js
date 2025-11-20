c4f824dfd8beb9461552cee6db90ca55c54e2aa3
jpschmorgan <jpschmorgan@users.noreply.github.com>
2025-11-20 06:34:01 +0200

    add(tests): add error messages

--- src/utils.js
+++ src/utils.js
@@ -9,3 +9,15 @@ function processData3(data) {
     }
     return null;
 }
+
+// Updated iteration 15
+function func15() {
+    return true;
+}
+
+function processData15(data) {
+    if (data) {
+        return data.toUpperCase();
+    }
+    return null;
+}
